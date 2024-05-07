aa51e3286f6b797f43e44474e17c2dbe3d55deaf
jamesasmithiii <james.a.smith.iii@gmail.com>
2024-05-07 13:56:06 -0400

    added timer, score and collision detection

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,9 +9,13 @@ class Game {
     this.background = new Background(this);
     this.player = new Player(this);
     this.obstacles = [];
-    this.numberOfObstacles = 10;
+    this.numberOfObstacles = 2;
     this.gravity;
     this.speed;
+    this.score;
+    this.timer;
+    this.message1;
+    this.message2;
 
     this.resize(window.innerWidth, window.innerHeight);
 
@@ -35,6 +39,10 @@ class Game {
     this.canvas.width = width;
     this.canvas.height = height;
     this.ctx.fillStyle = "blue";
+    this.ctx.font = "15px Bungee";
+    this.ctx.textAlign = "right";
+    this.ctx.lineWidth = 3;
+    this.ctx.strokeStyle = "white";
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.ratio = this.height / this.baseHeight;
@@ -47,10 +55,15 @@ class Game {
     this.obstacles.forEach((obstacle) => {
       obstacle.resize();
     });
+    this.score = 0;
+    this.gameOver = false;
+    this.timer = 0;
   }
-  render() {
+  render(deltaTime) {
+    if (!this.gameOver) this.timer += deltaTime;
     this.background.update();
     this.background.draw();
+    this.drawStatusText();
     this.player.update();
     this.player.draw();
     this.obstacles.forEach((obstacle) => {
@@ -67,6 +80,54 @@ class Game {
       this.obstacles.push(new Obstacle(this, firstX + i * obstacleSpacing));
     }
   }
+
+  checkCollision(a, b) {
+    const dx = a.collisionX - b.collisionX;
+    const dy = a.collisionY - b.collisionY;
+    const distance = Math.hypot(dx, dy);
+    const sumOfRadii = a.collisionRadius + b.collisionRadius;
+    return distance <= sumOfRadii;
+  }
+
+  formatTimer() {
+    return (this.timer * 0.001).toFixed(1);
+  }
+
+  drawStatusText() {
+    this.ctx.save();
+    this.ctx.fillText("Score: " + this.score, this.width - 10, 30);
+    this.ctx.textAlign = "left";
+    this.ctx.fillText("Timer: " + this.formatTimer(), 10, 30);
+    if (this.gameOver) {
+      if (this.player.collided) {
+        this.message1 = "Getting rusty?";
+        this.message2 = "Collision time " + this.formatTimer() + " seconds!";
+      } else if (this.obstacles.length <= 0) {
+        this.message1 = "Nailed it!";
+        this.message2 =
+          "Can you do it faster than " + this.formatTimer() + " seconds?";
+      }
+      this.ctx.textAlign = "center";
+      this.ctx.font = "30px Bungee";
+      this.ctx.fillText(
+        this.message1,
+        this.width * 0.5,
+        this.height * 0.5 - 40
+      );
+      this.ctx.font = "15px Bungee";
+      this.ctx.fillText(
+        this.message2,
+        this.width * 0.5,
+        this.height * 0.5 - 20
+      );
+      this.ctx.fillText(
+        "Press 'R' to try again!",
+        this.width * 0.5,
+        this.height * 0.5
+      );
+    }
+    this.ctx.restore();
+  }
 }
 
 window.addEventListener("load", function () {
@@ -77,9 +138,12 @@ window.addEventListener("load", function () {
 
   const game = new Game(canvas, ctx);
 
-  function animate() {
+  let lastTime = 0;
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.render();
+    game.render(deltaTime);
     requestAnimationFrame(animate);
   }
   requestAnimationFrame(animate);
